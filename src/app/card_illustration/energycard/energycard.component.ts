96b89256e976f30d6cddf89fcf1da18bc0933d18
Cihat Erdem <cihat_erdem@outlook.com>
2022-12-15 16:14:02 +0100

    cardComponents

---
+++ src/app/card_illustration/energycard/energycard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-energycard',
+  templateUrl: './energycard.component.html',
+  styleUrls: ['./energycard.component.css']
+})
+export class EnergycardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/card_illustration/trainercard/trainercard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainercard',
+  templateUrl: './trainercard.component.html',
+  styleUrls: ['./trainercard.component.css']
+})
+export class TrainercardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
