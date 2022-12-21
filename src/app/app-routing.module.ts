b667614fa7143bfb8a6f468cbb0b040d301a3bd7
Cihat Erdem <cihat_erdem@outlook.com>
2022-12-21 10:27:05 +0100

    update directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PostArticleComponent} from "./post-article/post-article.component";
+import {PostArticleComponent} from "./components/post-article/post-article.component";
 
 const routes: Routes = [
   {path: "post", component: PostArticleComponent}
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Article} from "../model/Article";
-import {ArticleService} from "../services/article.service";
+import {Article} from "../../model/Article";
+import {ArticleService} from "../../services/article.service";
 
 @Component({
   selector: 'app-article',
