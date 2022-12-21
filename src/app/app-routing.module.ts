bbcdeaced6937991cbf04f418cac762f67b2a1d0
Cihat Erdem <cihat_erdem@outlook.com>
2022-12-21 10:35:33 +0100

    update routing + show articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {PostArticleComponent} from "./components/post-article/post-article.component";
+import {ShowArticlesComponent} from "./components/show-articles/show-articles.component";
 
 const routes: Routes = [
-  {path: "post", component: PostArticleComponent}
+  {path: "post", component: PostArticleComponent},
+  {path: "show-articles", component: ShowArticlesComponent},
+
 ];
 
 @NgModule({
